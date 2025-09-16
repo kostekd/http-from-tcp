5064172e9438a3b949908e424e6443cbfb0130fc
kostekd <dominikkostencki99@gmail.com>
2025-09-16 13:06:43 +0200

    make it work

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func readFile(path string) *os.File {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err);
+	}
+	return file;
+}
+
+func getLinesChannel(file io.ReadCloser) <- chan string {
+	lines := make(chan string)
+	go func() {
+		buf := make([]byte, 8)
+		str := ""
+		for {
+			chunk, err := file.Read(buf)
+			if err != nil && err != io.EOF {
+				log.Fatal(err)
+			}
+	
+			if chunk == 0 {
+				break
+			}
+	
+			index := strings.Index(string(buf[:8]), "\n");
+			if index == -1 {
+				str += string(buf[:8]);
+			} else {
+				str += string(buf[:index + 1]	)
+				lines <- str
+				str = string(buf[index + 1:])
+			}
+		}
+		close(lines)
+	}()
+	return lines
+}
+
+
+func main() {
+	file := readFile("messages.txt")
+	channel := getLinesChannel(file)
+
+	for result := range channel {
+		fmt.Printf("read: %s", result);
+	}
+}
