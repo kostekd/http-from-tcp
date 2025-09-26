12046d7b50301c07d1a8acca7edb099079b3f58a
kostekd <dominikkostencki99@gmail.com>
2025-09-26 19:38:48 +0200

    add headers printing

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -24,6 +24,10 @@ func main() {
 		}
 
 		fmt.Printf("Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n", request.RequestLine.Method, request.RequestLine.RequestTarget, request.RequestLine.HttpVersion)
+		fmt.Println("Headers:")
+		for k, v := range request.Headers {
+			fmt.Printf("- %s: %s\n", k, v)
+		}
 		connection.Close()
 	}
 }
